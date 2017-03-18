fe6f0c1050132efe13e747d52293c7610e5a3276
AJ <juliustip@gmail.com>
2017-03-18 12:52:21 +0100

    Update index.php
    SSL Verification disabled due to PHP 7.0 inability to verify some ssl cert

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+//SSL Verification disabled due to PHP 7.0 inability to verify some ssl cert
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+);
+
+//Request code starts here
   header('Content-Type: application/json');
-  echo file_get_contents($_REQUEST['url']);
+  echo file_get_contents($_REQUEST['url'],false, stream_context_create($arrContextOptions));
 ?>
